7f19f9403d10504ba1bffe7d3172d0d21aef6649
Ilya Tyupo <derherrmannelig@gmail.com>
2023-12-30 10:56:17 +0200

    feat: add 1st test case

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,7 @@
 from playwright.sync_api import Page
 
+body = 'body'
+
 class Base():
   def __init__(self, page: Page):
     self.page = page
@@ -9,7 +11,13 @@ class Base():
     self.page.wait_for_load_state()
 
   def get(self, locator):
+    self.page.wait_for_load_state()
     return self.page.locator(locator)
 
   def click(self, locator):
+    self.page.wait_for_load_state()
     self.get(locator).click()
+
+  def getBody(self):
+    self.page.wait_for_load_state()
+    return self.get(body)
--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -2,6 +2,23 @@ import pytest
 
 from pages.authorization_page import Authorization
 from pages.base_page import Base
+from faker import Faker
+import random
+fake = Faker()
+
+nameSignup = fake.name()
+emailSignup = fake.email()
+passwordSignup = fake.password()
+genderSignup = random.randint(1, 2)
+fnamePersonal = fake.first_name()
+lnamePersonal = fake.last_name()
+companyPersonal = fake.bs()
+address1Personal = fake.address()
+address2Personal = fake.secondary_address()
+statePersonal = fake.state()
+cityPersonal = fake.city()
+zipcodePersonal = fake.zipcode()
+phonePersonal = fake.basic_phone_number()
 
 class TestAuthorization:
 
@@ -12,7 +29,27 @@ class TestAuthorization:
     self.base = Base(self.page)
     self.authorization = Authorization(self.page)
     self.base.url("")
-    assert self.page.url == 'https://automationexercise.com/'
+    assert self.base.getBody().is_visible()
 
   def test_register(self, test_setup):
-    print('smth')
+    self.authorization.clickLoginNav()
+    assert self.authorization.getSignupHeader().is_visible()
+    self.authorization.fillSignupForm(nameSignup, emailSignup)
+    self.authorization.clickButtonSignup()
+    assert self.authorization.getRegisterHeaders().first.is_visible()
+    day = random.randint(1, len(self.authorization.getDayOptions()) - 1)
+    month = random.randint(1, len(self.authorization.getMonthOptions()) - 1)
+    year = random.randint(1, len(self.authorization.getYearOptions()) - 1)
+    self.authorization.completeSignupForm(genderSignup, passwordSignup, day, month, year)
+    self.authorization.clickCheckboxNewsletter()
+    self.authorization.clickCheckboxOptin()
+    countryPersonal = random.randint(0, len(self.authorization.getCountryOptions()) - 1)
+    self.authorization.fillPersonalData(fnamePersonal, lnamePersonal, companyPersonal, address1Personal, address2Personal, countryPersonal, statePersonal, cityPersonal, zipcodePersonal, phonePersonal)
+    self.authorization.clickButtonCreateAccount()
+    assert self.authorization.getHeaderSignupSuccess().is_visible()
+    self.authorization.clickButtonContinue()
+    assert self.authorization.getUsername().is_visible()
+    assert self.authorization.getUsername().inner_text() == nameSignup
+    self.authorization.clickDeleteNav()
+    assert self.authorization.getHeaderDeleteSuccess().is_visible()
+    self.authorization.clickButtonContinue()
